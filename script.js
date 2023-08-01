6801feaec3383ba07be7f7d989757df176dea8d4
Danvhin5hs <Vramgallerdi1986puterxd@gmail.com>
2023-08-01 01:03:47 -0500

    añadido nuevo tipo de script dom para cambiar el texto de un boton

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("miBoton").addEventListener("click", function() {
+      this.textContent = "Texto Original";
+    });
+  });
