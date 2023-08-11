1071fcf4888dbad67062bdfd2fc56078fc17772e
Danvhin5hs <Vramgallerdi1986puterxd@gmail.com>
2023-08-11 00:55:59 -0500

    pista porque tengo que dar clic 2 veces

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-window.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("miBoton").addEventListener("click", function() {
-      this.textContent = "Texto Original";
-    });
+document.addEventListener("DOMContentLoaded", function() {
+
+  const button = document.querySelector("button")
+  
+  button.addEventListener("click", function(){
+    const id = button.getAttribute("data-id")
+  
+  if(button.classList.contains("liked")){
+    button.classList.remove("liked")
+    button.innerText ="Me gusta"
+    
+  }
+  else{
+    button.classList.add("liked")
+    button.innerText ="Quitar me gusta"
+  }
+  })
+  
   });
+  
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  const button = document.querySelector("button.verificar")
+  button.addEventListener("click", function(){
+  if(button.classList.contains("Listo")){   // revisar solo funciona cuando le doy 2 clics
+    button.classList.remove("Listo", "verificar")
+    button.innerText = "No listo"
+  }
+  else {
+    button.classList.remove("verificar") //pista, tenia que borrar la clase verificar antes de 
+                                         // añadir otra clase 
+    button.classList.add("Listo");
+    button.innerText = "Listo"; // Cambia el texto del botón a "Listo"
+  }
+  })
+})
