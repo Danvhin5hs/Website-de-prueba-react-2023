99dc69ba540b5ed61635dd089364f713e28626cf
Danvhin5hs <Vramgallerdi1986puterxd@gmail.com>
2023-08-17 01:14:03 -0500

    corregido error botones al presionar 2 veces

--- script.js
+++ script.js
@@ -24,14 +24,18 @@ document.addEventListener("DOMContentLoaded", function(){
 
   const button = document.querySelector("button.verificar")
   button.addEventListener("click", function(){
-  if(button.classList.contains("Listo")){   // revisar solo funciona cuando le doy 2 clics
-    button.classList.remove("Listo", "verificar")
+  if(button.classList.contains("verificar")){   // revisar solo funciona cuando le doy 2 clics
+    button.classList.remove("verificar")
+    button.classList.add("Listo");
     button.innerText = "No listo"
   }
+ 
+  
+ 
   else {
-    button.classList.remove("verificar") //pista, tenia que borrar la clase verificar antes de 
+    button.classList.remove("Listo") //pista, tenia que borrar la clase verificar antes de 
                                          // añadir otra clase 
-    button.classList.add("Listo");
+    button.classList.add("verificar");
     button.innerText = "Listo"; // Cambia el texto del botón a "Listo"
   }
   })
